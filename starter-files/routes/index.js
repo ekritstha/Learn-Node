e74fc6ed1a0e1c7077e09fefcc04cfb782dcf472
Akrit Shrestha <akrit.act360@gmail.com>
2020-09-09 20:18:04 +0545

    routing and template(pug)

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -1,9 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+router.get("/", (req, res) => {
+  // res.send("Hey! It works!");
+
+  // const animal = { name: "dog", age: 9 };
+  // res.json(animal);
+
+  //to get from url use req
+  // res.send(req.query.name);
+  // res.json(req.query);
+  res.render("hello", {
+    name: "akrit",
+    dog: "kale",
+  });
+});
+
+router.get("/reverse/:name", (req, res) => {
+  const reverse = [...req.params.name].reverse().join("");
+  res.send(reverse);
 });
 
 module.exports = router;
