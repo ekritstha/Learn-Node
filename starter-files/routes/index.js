f613da0ac6fc645ac133844c20374be8d8b14ce1
Akrit Shrestha <akrit.act360@gmail.com>
2020-09-18 22:58:15 +0545

    wedit store

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -1,25 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const storeController = require("../controllers/storeController");
+const { catchErrors } = require("../handlers/errorHandlers");
 
-// Do work here
-router.get("/", (req, res) => {
-  // res.send("Hey! It works!");
-
-  // const animal = { name: "dog", age: 9 };
-  // res.json(animal);
-
-  //to get from url use req
-  // res.send(req.query.name);
-  // res.json(req.query);
-  res.render("hello", {
-    name: "akrit",
-    dog: "kale",
-  });
-});
-
-router.get("/reverse/:name", (req, res) => {
-  const reverse = [...req.params.name].reverse().join("");
-  res.send(reverse);
-});
+router.get("/", catchErrors(storeController.getStores));
+router.get("/stores", catchErrors(storeController.getStores));
+router.get("/add", storeController.addStore);
+router.post("/add", catchErrors(storeController.createStore));
+router.post("/add/:id", catchErrors(storeController.updateStore));
+router.get("/stores/:id/edit", catchErrors(storeController.editStore));
 
 module.exports = router;
--- starter-files/start.js
+++ starter-files/start.js
@@ -24,6 +24,9 @@ mongoose.connection.on("error", (err) => {
 
 // READY?! Let's go!
 
+//import all models
+require("./models/Store");
+
 // Start our app!
 const app = require("./app");
 app.set("port", process.env.PORT || 7777);
