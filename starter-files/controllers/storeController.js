fa008d9fe616c744e0e9353b543d42252bf4de89
Akrit Shrestha <akrit.act360@gmail.com>
2020-10-10 15:13:37 +0545

    uploading photo(mdule 18)

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -1,6 +1,21 @@
 const { raw } = require("body-parser");
 const mongoose = require("mongoose");
 const Store = mongoose.model("Store");
+const multer = require("multer");
+const jimp = require("jimp");
+const uuid = require("uuid");
+
+const multerOptions = {
+  storage: multer.memoryStorage(),
+  fileFilter(req, file, next) {
+    const isPhoto = file.mimetype.startsWith("image/");
+    if (isPhoto) {
+      next(null, true);
+    } else {
+      next({ message: "That filetype isn't alowed!" }, false);
+    }
+  },
+};
 
 exports.homePage = (req, res) => {
   res.render("index");
@@ -8,6 +23,24 @@ exports.homePage = (req, res) => {
 
 exports.addStore = (req, res) => {
   res.render("editStore", { title: "ADD STORE" });
+};
+
+exports.upload = multer(multerOptions).single("photo");
+
+exports.resize = async (req, res, next) => {
+  if (!req.file) {
+    next();
+    return;
+  }
+  const extension = req.file.mimetype.split("/")[1];
+  req.body.photo = `${uuid.v4()}.${extension}`;
+
+  //now resizing
+  const photo = await jimp.read(req.file.buffer);
+  await photo.resize(800, jimp.AUTO);
+  await photo.write(`./public/uploads/${req.body.photo}`);
+  //once uploaded,cntinue
+  next();
 };
 
 exports.createStore = async (req, res) => {
@@ -30,6 +63,7 @@ exports.editStore = async (req, res) => {
 };
 
 exports.updateStore = async (req, res) => {
+  // req.body.location.type = "Point";
   const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
     new: true,
     runValidators: true,
